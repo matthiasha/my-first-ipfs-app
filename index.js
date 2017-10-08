02f94ca2f632e1c05b58dbd74a922a2cf2b6d14c
Matthias Hafner <matthias.hafner@dolby.com>
2017-10-08 15:29:49 +0200

    add IPFS file upload and display link/img in feed

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const OrbitDB = require('orbit-db')
 
 const elm = document.getElementById("output")
 const openButton = document.getElementById("open")
+const uploadButton = document.getElementById("upload")
 
 const openDatabase = () => {
   elm.innerHTML = "Starting IPFS..."
 
-  const dbname = "QmbVycmVAsdeP8ocXrF8yGDhiPdpbXocFhSu3Jtrfspfdx" //todo
+  const dbname = "feed"
   const username = new Date().getTime()
   const ipfs = new IPFS({
     IpfsDataDir: '/orbit-db-/examples/browser',
@@ -27,27 +28,48 @@ const openDatabase = () => {
     elm.innerHTML = "Loading database..."
 
     const orbit = new OrbitDB(ipfs, username)
-    //const log = orbit.eventlog(dbname + ".log", { maxHistory: 5, syncHistory: false, cachePath: '/orbit-db' })
-    const log = orbit.eventlog(dbname + ".log", { cachePath: '/orbit-db' })
+    const log = orbit.eventlog(dbname, { cachePath: '/orbit-db' })
 
     const interval = Math.floor((Math.random() * 5000) + 3000)
 
-    const query = () => {
-      const value = document.getElementById("dbname").value
+    function addlog(value) {
       log.add(value)
         .then(() => getData())
         .catch((e) => handleError(e))
     }
+
+    const query = () => {
+        const value = document.getElementById("dbname").value
+        addlog(value)
+    }
     openButton.addEventListener('click', query)
+
+
+    function upload() {
+        let file = uploadButton.files[0]
+        let reader = new window.FileReader()
+        reader.onloadend = () => {
+            const buffer = Buffer.from(reader.result)
+            ipfs._daemon.files.add(buffer)
+                .then((response) => {
+                    let hash = response[0].hash
+                    if (file.type.startsWith("image/")) {
+                        addlog("<img src='/ipfs/" + hash + "' />")
+                    } else {
+                        addlog("<a href='/ipfs/" + hash + "' >" + hash + "</a>")
+                    }
+                }).catch((err) => {
+                    console.error(err)
+                })
+        }
+        reader.readAsArrayBuffer(file)
+    }
+
+    uploadButton.addEventListener('change', upload)
     
     function handleLogEntry(e) {
         const v = e.payload.value
-        if (v.length == 46) {
-            // assume we got an image hash
-            return "<img src='/ipfs/" + v + "' />"
-        } else {
-            return v
-        }
+        return v
     }
     
     const getData = () => {
